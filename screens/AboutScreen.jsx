e42ca28e072453efdd628fd80d5db9f3a6362fcb
B31rut <daniel.santiago0302@gmail.com>
2024-04-18 00:47:17 -0400

    Added ability to like campsite in campsite info screen, but when you leave the campsite info screen and come back, it still has the like button liked

--- screens/AboutScreen.jsx
+++ screens/AboutScreen.jsx
@@ -2,6 +2,7 @@ import { ScrollView, View, Text, FlatList } from "react-native";
 import { Card, ListItem, Avatar } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../.expo/components/LoadingComponent";
 
 const Mission = () => {
   return (
@@ -25,6 +26,33 @@ const Mission = () => {
 
 const AboutScreen = () => {
   const partners = useSelector((state) => state.partners);
+
+  if (partners.isLoading) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Loading />
+        </Card>
+      </ScrollView>
+    );
+  }
+
+  if (partners.errMess) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Text>{partners.errMess}</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
+
   return (
     <ScrollView>
       <Mission />
